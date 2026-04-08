7dc1edf85651dd749cfc72e62202487f3b41b37a
maerd47 <78052898+maerd47@users.noreply.github.com>
2026-04-08 16:55:40 -0400

    updated

--- flask_app/app.py
+++ flask_app/app.py
@@ -124,7 +124,7 @@ model_version = get_latest_model_version(model_name)
 model_uri = f'models:/{model_name}/{model_version}'
 print(f"Fetching model from: {model_uri}")
 model = mlflow.pyfunc.load_model(model_uri)
-vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
 
 # Routes
 @app.route("/")
