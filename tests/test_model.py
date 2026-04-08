bd50f3c06e85e25f2a7c3dc77dc4366941985182
maerd47 <78052898+maerd47@users.noreply.github.com>
2026-04-07 23:03:17 -0400

    updated

--- tests/test_model.py
+++ tests/test_model.py
@@ -20,8 +20,8 @@ class TestModelLoading(unittest.TestCase):
         os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
         dagshub_url = "https://dagshub.com"
-        repo_owner = "vikashdas770"
-        repo_name = "YT-Capstone-Project"
+        repo_owner = "buzzworm47"
+        repo_name = "End-to-end-imdb-MLops"
 
         # Set up MLflow tracking URI
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
